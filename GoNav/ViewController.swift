10c5c001abdda74bc632f250ff5aebfd2cf94330
user234693 <user234693@NY545.local>
2023-11-07 13:26:10 -0500

    map added

--- GoNav/ViewController.swift
+++ GoNav/ViewController.swift
@@ -37,6 +37,13 @@ class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDeleg
     @IBOutlet weak var bottomBar: UIView!
     
     var locationManager = CLLocationManager()
+    var tripStarted = false
+    var startTime: Date?
+    var totalDistance: CLLocationDistance = 0
+    var MaxSpeed: CLLocationSpeed = 0
+    var MaxAcceleration: Double = 0
+    var previousSpeed: CLLocationSpeed = 0
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,6 +55,52 @@ class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDeleg
         mapView.userTrackingMode = .follow
     }
 
+    
+    @IBAction func stopTrip(_ sender: UIButton) {
+        tripStarted = false
+        startTime = nil
+    }
+    
+    @IBAction func startTrip(_ sender: UIButton) {
+        if !tripStarted{
+            tripStarted = true
+            startTime = Date()
+            totalDistance = 0
+            MaxSpeed = 0
+            MaxAcceleration = 0
+            previousSpeed = 0
+            previousLocation = nil
+        }
+        
+        
+        func manageLocation(_ manager:CLLocationManager, didUpdateLocations locations: [CLLocation]){
+            
+            guard let location = locations.last else {return}
+            
+            if tripStarted{
+                
+                
+                if let previousLocation = previousLocation, let startTime = startTime {
+                    
+                    let timeElapsed = location.timestamp.timeIntervalSince(startTime)
+                        let distance = location.distance(from: previousLocation)
+                        
+                        let speed = location.speed
+                        totalDistance += distance
 
-}
+                        if speed > MaxSpeed{
+                            MaxSpeed = speed
+                        }
+                    }
+                }
+            
+                
+        }
+        
+        
+        
+        
+    }
+    
+    }
 
